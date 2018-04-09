d77e64b909dea8f6efec4a0c8dc78710b7b248a4
e <lvsanli@fangjia.com>
2018-04-09 11:40:18 +0800

    build

--- fanggugu/get_building_info.py
+++ fanggugu/get_building_info.py
@@ -60,7 +60,6 @@ class GetBuild(object):
                     response = requests.post(url='http://192.168.0.235:8999/send_proxy_status', data=formdata)
                     status = response.text
                     print(status)
-            print(result.text)
             if 'true' in result.text or 'True' in result.text:
                 building_list = json.loads(result.text)['list']
                 if not building_list:
